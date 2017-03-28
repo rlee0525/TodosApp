ffecde26f8203a357161a1e0a90824faa22e4722
Raymond Lee <raylee0525@gmail.com>
2017-03-28 14:46:38 -0700

    Implement update and delete, with checkboxes

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -26,6 +26,18 @@ class TodosListCtrl {
 
     this.newTask = '';
   }
+
+  removeTask(task) {
+    Tasks.remove(task._id);
+  }
+
+  setChecked(task) {
+    Tasks.update(task._id, {
+      $set: {
+        checked: !task.checked
+      },
+    });
+  }
 }
 
 export default angular.module('todosList', [
