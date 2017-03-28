a879bfb6a69687d908d1e6d88492ae743e69e4d6
Raymond Lee <raylee0525@gmail.com>
2017-03-28 13:36:41 -0700

    Implement MongoDB

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -1,16 +1,17 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import template from './todosList.html';
+import { Tasks } from '../../api/tasks';
 
 class TodosListCtrl {
-  constructor() {
-    this.tasks = [{
-      text: 'This is task 1'
-    }, {
-      text: 'This is task 2'
-    }, {
-      text: 'This is task 3'
-    }];
+  constructor($scope) {
+    $scope.viewModel(this);
+
+    this.helpers({
+      tasks() {
+        return Tasks.find({});
+      }
+    })
   }
 }
 
@@ -19,5 +20,5 @@ export default angular.module('todosList', [
 ])
   .component('todosList', {
     templateUrl: 'imports/components/todosList/todosList.html',
-    controller: TodosListCtrl
+    controller: ['$scope', TodosListCtrl]
   });
