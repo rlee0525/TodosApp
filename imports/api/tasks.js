c43003352383e8e5e0e313d1fbc336acdc69f09b
Raymond Lee <raylee0525@gmail.com>
2017-03-29 11:43:33 -0700

    Secure the app to require login to edit DB and pull information from FB

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -1,3 +1,45 @@
+import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
+import { check } from 'meteor/check';
 
 export const Tasks = new Mongo.Collection('tasks');
+
+Meteor.methods({
+ 'tasks.insert' (text) {
+   check(text, String);
+
+   if (!Meteor.userId()) {
+     throw new Meteor.Error('not-authorized');
+   }
+
+   Tasks.insert({
+     text,
+     createdAt: new Date(),
+     owner: Meteor.userId(),
+     name: Meteor.user().services.facebook.name,
+   });
+ },
+ 'tasks.remove' (taskId) {
+   check(taskId, String);
+
+   if (!Meteor.userId()) {
+     throw new Meteor.Error('not-authorized');
+   }
+
+   Tasks.remove(taskId);
+ },
+ 'tasks.setChecked' (taskId, setChecked) {
+   check(taskId, String);
+   check(setChecked, Boolean);
+
+   if (!Meteor.userId()) {
+     throw new Meteor.Error('not-authorized');
+   }
+
+   Tasks.update(taskId, {
+     $set: {
+       checked: setChecked
+     }
+   });
+ },
+});
