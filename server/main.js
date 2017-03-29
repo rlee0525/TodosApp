0b1e6ecbd77869ba333e77db332173d01cbf9f5b
Raymond Lee <raylee0525@gmail.com>
2017-03-29 11:20:53 -0700

    Implement facebook oauth

--- server/main.js
+++ server/main.js
@@ -1,1 +1,11 @@
 import '../imports/api/tasks.js';
+
+ServiceConfiguration.configurations.remove({
+    service: "facebook"
+});
+
+ServiceConfiguration.configurations.insert({
+    service: "facebook",
+    appId: '1663364747300025',
+    secret: '197749adc76bfaac7c54135070942489'
+});
